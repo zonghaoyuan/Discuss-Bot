fe1dc7393deeb9e1b0534a62c6f15936ea262019
Winston Lee <liyouranmail@gmail.com>
2024-08-20 14:08:21 +0800

    Update reply_generator.py

--- config/reply_generator.py
+++ config/reply_generator.py
@@ -1,4 +1,33 @@
+import os
+import platform
 import random
+import requests
+from configparser import ConfigParser
+import io
+import logging
+
+# 自动判断运行环境
+IS_GITHUB_ACTIONS = 'GITHUB_ACTIONS' in os.environ
+IS_SERVER = platform.system() == "Linux" and not IS_GITHUB_ACTIONS
+
+# 从配置文件或环境变量中读取配置信息
+def load_config():
+    config = ConfigParser()
+    if IS_SERVER:
+        config_file = './config.ini'
+    elif IS_GITHUB_ACTIONS:
+        config_file = None
+    else:
+        config_file = 'config.ini'
+    
+    if config_file and os.path.exists(config_file):
+        config.read(config_file)
+    
+    return config
+
+config = load_config()
+
+HITOKOTO_URL = config.get('urls', 'hitokoto_url', fallback="https://v1.hitokoto.cn/")
 
 # 扩充的句型模板
 sentence_templates = [
@@ -56,6 +85,7 @@ def generate_positive_sentence():
         position = random.randint(0, len(sentence))
         sentence = sentence[:position] + random.choice(emojis) + sentence[position:]
     # 添加随机图片链接到句子的下一行
+    sentence += "\n" + get_hitokoto()
     sentence += "\n" + generate_random_image_url()
     return sentence
 
@@ -68,14 +98,35 @@ def load_predefined_replies():
     except FileNotFoundError:
         return []
 
+def get_hitokoto() -> str:
+    """
+    从 API 获取一条一言，并返回格式化的字符串。
+
+    Returns:
+        str: 格式化的一言字符串，如 "憧憬，是距离了解最遥远的一种感情。    ----BLEACH"
+    """
+    try:
+        response = requests.get(HITOKOTO_URL)
+        response.raise_for_status()  # 如果响应状态不是 200，则抛出 HTTPError 异常
+        data = response.json()
+        hitokoto = data["hitokoto"]
+        from_source = data["from"]
+        return f"{hitokoto}    ----{from_source}"
+    except requests.RequestException as e:
+        logging.error(f"一条一言获取失败: {e}。")
+        return ""
+
 def generate_or_load_reply():
     """随机选择生成或读取一条回复"""
     predefined_replies = load_predefined_replies()
     if random.choice([True, False]) and predefined_replies:
-        return random.choice(predefined_replies) + "\n" + generate_random_image_url()
+        return random.choice(predefined_replies) + "\n" + get_hitokoto() + "\n" + generate_random_image_url()
     else:
         return generate_positive_sentence()
 
 def get_random_reply():
     """外部调用直接获取一条随机生成的回复"""
     return generate_or_load_reply()
+
+# 示例调用
+# print(generate_or_load_reply())
